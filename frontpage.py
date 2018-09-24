1d6a0b422c9d8f046684b86075c515497a389a8e
Michael Hippke <hippke@users.noreply.github.com>
2018-09-24 16:06:56 +0200

    Update frontpage.py

--- frontpage.py
+++ frontpage.py
@@ -9,17 +9,19 @@ import batman
 
 if __name__ == '__main__':
     
-
+    numpy.random.seed(seed=2)  # reproducibility 
+    # 2: points look OK; 15.6/15.8 (10ppm, 24)
+    # 0: points mhm; 10.2/10.3 (10ppm, 24)
     # Create test data
     start = 0
-    days = 365.25 * 3.5  # change this and it breaks
-    samples_per_day = 12  # 48
+    days = 365.25 * 3
+    samples_per_day = 24  # 48
     samples = int(days * samples_per_day) # 48
     t = numpy.linspace(start, start + days, samples)
 
     # Use batman to create transits
     ma = batman.TransitParams()
-    ma.t0 = start + 50 # time of inferior conjunction; first transit is X days after start
+    ma.t0 = start + 5 # time of inferior conjunction; first transit is X days after start
     ma.per = 365.25  # orbital period
     ma.rp = 6371 / 696342  # planet radius (in units of stellar radii)
     ma.a = 217  # semi-major axis (in units of stellar radii)
@@ -46,13 +48,13 @@ if __name__ == '__main__':
         R_star=1,
         M_star=1,
         time_span=(max(t) - min(t)),
-        period_min=365,  # 10.04
-        period_max=365.5,  # 10.05
-        oversampling_factor=3)
+        period_min=0,  # 10.04
+        period_max=500,  # 10.05
+        oversampling_factor=5)
 
     # Define grids of transit depths and widths
-    depths = numpy.geomspace(50*10**-6, 150*10**-6, 20)  # 50
-    durations = numpy.geomspace(0.001, 0.002, 20)  # 50
+    depths = numpy.geomspace(90*10**-6, 110*10**-6, 10)  # 50
+    durations = numpy.geomspace(0.0013, 0.0018, 5)  # 50
 
     model = TransitLeastSquares(t, y, dy)
     results = model.power(periods, durations, depths, limb_darkening=0.5) # likelihood  # snr
@@ -64,50 +66,108 @@ if __name__ == '__main__':
     print('Best duration (fractional period)', format(results.best_duration, '.5f'))
     print('Best duration (days)', format(results.transit_duration_in_days, '.5f'))
     print('Signal detection efficiency (SDE):', results.SDE)
-
-    #print(results.power)
     
-    # Test statistic
-    plt.figure(figsize=(4.5, 4.5 / 1.5))
-    ax = plt.gca()
-    ax.axvline(results.best_period, alpha=0.4, lw=3)
-    plt.xlim(numpy.min(periods), numpy.max(periods))
-    for n in range(2, 10):
-        ax.axvline(n*results.best_period, alpha=0.4, lw=1, linestyle="dashed")
-        ax.axvline(results.best_period / n, alpha=0.4, lw=1, linestyle="dashed")
-    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
-    plt.ylabel(r'SDE')
-    plt.xlabel('Period (days)')
-    plt.savefig('fig_test_stat_raw.pdf', bbox_inches='tight')
-    
-    
-    # Folded model
+    # Final plot
     phases = fold(t, results.best_period, T0=results.best_T0+ results.best_period/2)
-    #phases = fold(t, 1.796, T0=0)
     sort_index = numpy.argsort(phases)
     phases = phases[sort_index]
     flux = y[sort_index]
-    plt.figure(figsize=(4.5, 4.5 / 1.5))
-    plt.scatter(phases, flux, color='blue', s=10, alpha=0.5, zorder=2)
-    plt.plot(numpy.linspace(0, 1, numpy.size(results.folded_model)), 
-        results.folded_model, color='red', zorder=3)
-    plt.ylim(0.99985, 1.00005)
-    plt.xlim(0.498, 0.502)
-    plt.xlabel('Phase')
-    plt.ylabel('Relative flux (ppm)')
-    plt.savefig('fig_test_fold.pdf', bbox_inches='tight')
-    
-    """
-    # Final plot
+    plt.figure(figsize=(8, 6))
+
+
     plt.subplot(221)
-    plt.plot(data[0])
-    # 2x2, second axis
-    plt.subplot(222)
-    plt.plot(data[1])
-    # 2x2, third axis
+    plt.subplots_adjust(wspace=0.3, hspace=0.3)
+    plt.errorbar(numpy.linspace((-results.best_period/2)*24, (results.best_period/2)*24, numpy.size(results.folded_model)), (flux-1)*10**6, yerr=ppm, fmt='o', color='red',
+        alpha=0.5, markersize=1, zorder=2)
+    plt.plot(numpy.linspace((-results.best_period/2)*24, (results.best_period/2)*24, numpy.size(results.folded_model)),
+        (results.folded_model-1)*10**6, color='blue', zorder=0)
+    plt.ylim(-120, 20)
+    plt.xlim(-15, 15)
+    plt.ylabel(r'Flux (ppm)')
+    plt.xlabel('Time from mid-transit (days)')
+
     plt.subplot(223)
-    plt.plot(data[2])
-    # 2x2, fourth axis
+    plt.axvline(results.best_period, alpha=0.4, lw=3)
+    for n in range(2, 10):
+        plt.axvline(n*results.best_period, alpha=0.4, lw=1, linestyle="dashed")
+        plt.axvline(results.best_period / n, alpha=0.4, lw=1, linestyle="dashed")
+    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    plt.ylabel(r'SDE')
+    plt.xlabel('Period (days)')
+    plt.xlim(0, 500)
+    max_sde_ld = results.SDE
+    scale = 1.1
+    max_plot_height = max_sde_ld * scale
+    plt.ylim(-1, max_plot_height)
+    plt.text(350, max_sde_ld, r'SDE$_{\rm peak}=$' + str(round(results.SDE, 1)), ha='right')
+
+
+    # Box
+    model = TransitLeastSquares(t, y, dy)
+    results = model.power(periods, durations, depths, limb_darkening=0.0) # likelihood  # snr
+
+    plt.subplot(222)
+
+    # Make the box look like a box when plotting
+    #modelflux = (results.folded_model-1)*10**6
+    #depth = results.best_depth
+    # Left
+    plt.plot(
+        ((-results.best_period/2)*24, -(results.transit_duration_in_days/2)*24),
+        (0, 0),
+        color='blue',
+        zorder=10)
+    # Right
+    plt.plot(
+        ((+results.best_period/2)*24, +(results.transit_duration_in_days/2)*24),
+        (0, 0),
+        color='blue',
+        zorder=10)
+    # Bottom
+    plt.plot(
+        ((-(results.transit_duration_in_days/2)*24, +(results.transit_duration_in_days/2)*24)),
+        ((-results.best_depth)*10**6, (-results.best_depth)*10**6),
+        color='blue',
+        zorder=10)
+    # Left
+    plt.plot(
+        ((-(results.transit_duration_in_days/2)*24, -(results.transit_duration_in_days/2)*24)),
+        (0, (-results.best_depth)*10**6),
+        color='blue',
+        zorder=10)
+    # Right
+    plt.plot(
+        ((+(results.transit_duration_in_days/2)*24, +(results.transit_duration_in_days/2)*24)),
+        ((-results.best_depth)*10**6, 0),
+        color='blue',
+        zorder=10)
+
+    print((results.best_depth)*10**6)
+
+
+
+    plt.errorbar(numpy.linspace((-results.best_period/2)*24, (results.best_period/2)*24, numpy.size(results.folded_model)), (flux-1)*10**6, yerr=ppm, fmt='o', color='red',
+        alpha=0.5, markersize=1, zorder=2)
+    #plt.plot(numpy.linspace((-results.best_period/2)*24, (results.best_period/2)*24, numpy.size(results.folded_model)),
+    #    (results.folded_model-1)*10**6, color='blue', zorder=0)
+
+    plt.ylim(-120, 20)
+    plt.xlim(-15, 15)
+    #plt.ylabel(r'Flux (ppm)')
+    plt.xlabel('Time from mid-transit (days)')
+
     plt.subplot(224)
-    plt.plot(data[3])
-    """
+    plt.axvline(results.best_period, alpha=0.4, lw=3)
+    for n in range(2, 10):
+        plt.axvline(n*results.best_period, alpha=0.4, lw=1, linestyle="dashed")
+        plt.axvline(results.best_period / n, alpha=0.4, lw=1, linestyle="dashed")
+    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    #plt.ylabel(r'SDE')
+    plt.xlabel('Period (days)')
+    plt.xlim(0, 500)
+    plt.ylim(-1, max_plot_height)
+    plt.text(350, max_sde_ld, r'SDE$_{\rm peak}=$' + str(round(results.SDE, 1)), ha='right')
+
+    plt.savefig('frontpage.pdf', bbox_inches='tight')
