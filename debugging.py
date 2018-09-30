2f1fbb753d2675f459a67684e9390a2f7972bb70
Michael Hippke <hippke@users.noreply.github.com>
2018-09-30 21:57:12 +0200

    Update debugging.py

--- debugging.py
+++ debugging.py
@@ -3,11 +3,13 @@ import numpy
 #import scipy
 import matplotlib.pyplot as plt
 import batman
-from TransitLeastSquares import TransitLeastSquares, period_grid, fold, \
+#from TransitLeastSquares_cuda_multi_ootr2 import TransitLeastSquares, period_grid, fold
+from TransitLeastSquares_v22 import TransitLeastSquares, period_grid, fold,\
     get_duration_grid, get_depth_grid
+
 #from TransitLeastSquares import TransitLeastSquares, period_grid, fold
-#from astropy.stats import BoxLeastSquares
-from math import pi 
+from astropy.stats import BoxLeastSquares
+
 #from numba import cuda, autojit, float32
 
 if __name__ == "__main__":
@@ -22,7 +24,7 @@ if __name__ == "__main__":
 
     # Use batman to create transits
     ma = batman.TransitParams()
-    ma.t0 = start + 5 # time of inferior conjunction; first transit is X days after start
+    ma.t0 = start + 5  #5 # time of inferior conjunction; first transit is X days after start
     ma.per = 365.25  # orbital period
     ma.rp = 6371 / 696342  # planet radius (in units of stellar radii)
     ma.a = 217  # semi-major axis (in units of stellar radii)
@@ -35,7 +37,7 @@ if __name__ == "__main__":
     original_flux = m.light_curve(ma)  # calculates light curve
 
     # Create noise and merge with flux
-    ppm = 20
+    ppm = 1
     stdev = 10**-6 * ppm
     noise = numpy.random.normal(0, stdev, int(samples))
     y = original_flux + noise
@@ -43,9 +45,9 @@ if __name__ == "__main__":
     if numpy.std(noise) == 0:
         dy = numpy.full_like(y, 1)
     else:
-        dy = numpy.full_like(y, numpy.std(noise))
+        dy = numpy.full_like(y, stdev)#numpy.std(noise))
 
-    """
+
     fig, ax = plt.subplots(1, 1, sharex=True, figsize=(6, 3))
     ax.plot(t, y, "k")
     ax.set_xlim(t.min(), t.max())
@@ -53,49 +55,57 @@ if __name__ == "__main__":
     ax.set_ylabel("relative flux [ppt]")
     plt.savefig('dfm_01_allflux.pdf', bbox_inches='tight')
     #plt.show()
-    """
 
     y_filt = y
     t = t #- min(t) # + 100
     print('Samples', numpy.size(y), 'duration', max(t)-min(t))
     #print()
-
+    
     periods = period_grid(
         R_star=1,  # R_sun
         M_star=1,  # M_sun
         time_span=(max(t) - min(t)),  # days
-        period_min=300,  # 365.25,   360: OK; 370: error dip
-        period_max=400,   # 365.3,
+        period_min=364,
+        period_max=366,
         oversampling_factor=3)
-
-    durations = get_duration_grid(periods)
-    print(len(durations), 'durations from', format(min(durations), '.6f'), \
-            'to', format(max(durations), '.6f'))
-    depths = get_depth_grid(y)
-    print(len(depths), 'depths from', format(min(depths), '.6f'), \
-            'to', format(max(depths), '.6f'))
+    #periods = numpy.linspace(365.2, 365.3, 100)
 
 
     # Define grids of transit depths and widths
-    #depths = numpy.geomspace(10*10**-6, 1000*10**-6, 20)  # 50
-    #durations = numpy.geomspace(0.001, 0.003, 20)  # 50
+    #depths = numpy.geomspace(1*10**-6, 120*10**-6, 50)  # 50
+    #durations = numpy.geomspace(0.0015, 0.0020, 10)  # 50
+
+    durations = get_duration_grid(periods, log_step=1.1)
+    print(len(durations), 'durations from', min(durations), 'to', max(durations))
+    depths = get_depth_grid(y, log_step=1.1)
+    #depths = numpy.linspace(50*10**-6, 0.01, 50)
+    #print('depths', depths)
+    print(len(depths), 'depths from', min(depths), 'to', max(depths))
 
     model = TransitLeastSquares(t, y_filt, dy)
     results = model.power(periods, durations, depths, limb_darkening=0.5)
+    print('chi2min,max', min(results.chi2), max(results.chi2))
+    print('SDE TLS', max(results.chi2))
+    print(results.transit_times)
+    print(results.transit_snrs)
+
+    #model = TransitLeastSquares(t, y_filt, dy)
+    #results = model.power(periods, durations, depths, limb_darkening=0.0)
+    #print('SDE BLS', max(results.SDE_power))
 
     #from astropy.io import fits
     #from astropy import units as u
     #periods = numpy.linspace(200, 500, 10000) #* u.day
-    #import time
-    #t1 = time.perf_counter()
-    #bls_model = BoxLeastSquares(t, y_filt)
+    import time
+    t1 = time.perf_counter()
+    bls_model = BoxLeastSquares(t, y_filt)
     #durations = numpy.linspace(0.05, 0.2, 10) #* u.day
-    #results_bls = bls_model.power(periods, 0.2)
-    #t2 = time.perf_counter()
-    #print(t2-t1)
+    results_bls = bls_model.power(periods, numpy.linspace(0.1, 0.7, 20))
+    t2 = time.perf_counter()
+    print('BLS time', t2-t1)
     #results_bls = bls_model.autopower(durations, frequency_factor=3.0)
 
-    
+    """
     print('Period', format(results.best_period, '.5f'), 'd')
     print(len(results.transit_times), 'transit times in time series:', \
             ['{0:0.5f}'.format(i) for i in results.transit_times])
@@ -120,15 +130,15 @@ if __name__ == "__main__":
     #plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
     plt.plot(results_bls.period, results_bls.power, "k", lw=0.5)
     #ax.text(15, 60, round(max(results.SDE_power),1))
-    plt.ylabel(r'SDE')
+    plt.ylabel(r'BLS Teststatistik')
     plt.xlabel('Period (days)')
     #plt.show()
-    plt.savefig('test_stat_bls.pdf', bbox_inches='tight')
-    """
+
+    plt.savefig('BLS_T0_180d.pdf', bbox_inches='tight')
 
 
-    #plt.rc('font',  family='serif', serif='Computer Modern Roman')
-    #plt.rc('text', usetex=True)
+    plt.rc('font',  family='serif', serif='Computer Modern Roman')
+    plt.rc('text', usetex=True)
     plt.figure(figsize=(3.75, 3.75 / 1.5))
     ax = plt.gca()
     ax.get_yaxis().set_tick_params(which='both', direction='out')
@@ -140,14 +150,39 @@ if __name__ == "__main__":
     #for n in range(2, 10):
     #    ax.axvline(n*results.best_period, alpha=0.4, lw=1, linestyle="dashed")
     #    ax.axvline(results.best_period / n, alpha=0.4, lw=1, linestyle="dashed")
-    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    plt.plot(results.periods, results.chi2red, color='black', lw=0.5)
     #plt.plot(results_bls.period, results_bls.power, "k", lw=0.5)
     #ax.text(15, 60, round(max(results.SDE_power),1))
-    plt.ylabel(r'SDE')
+    plt.ylabel(r'$\chi^2_{\rm red}$')
+    #plt.ylabel(r'$\chi^2$')
     plt.xlabel('Period (days)')
     #plt.show()
-    plt.savefig('test_stat_tls.pdf', bbox_inches='tight')
+    plt.ylim(1, max(results.chi2red)*1.1)
+    plt.savefig('test_stat_tls_chi2red_ld0.pdf', bbox_inches='tight')
+    print(min(results.SDE_power), max(results.chi2red))
 
+    plt.rc('font',  family='serif', serif='Computer Modern Roman')
+    plt.rc('text', usetex=True)
+    plt.figure(figsize=(3.75, 3.75 / 1.5))
+    ax = plt.gca()
+    ax.get_yaxis().set_tick_params(which='both', direction='out')
+    ax.get_xaxis().set_tick_params(which='both', direction='out')
+    ax.axvline(results.best_period, alpha=0.4, lw=3)
+    plt.xlim(numpy.min(periods), numpy.max(periods))
+    #plt.xlim(0, 40)
+    plt.ylim(0, max(results.SDE_power)*1.1)
+    for n in range(2, 10):
+        ax.axvline(n*results.best_period, alpha=0.4, lw=1, linestyle="dashed")
+        ax.axvline(results.best_period / n, alpha=0.4, lw=1, linestyle="dashed")
+    plt.plot(results.periods, results.SDE_power, color='black', lw=0.5)
+    #plt.plot(results_bls.period, results_bls.power, "k", lw=0.5)
+    ax.text(results.best_period, results.SDE, round(max(results.SDE_power),1))
+    #lt.ylabel(r'$\chi^2_{\rm red}$')
+    plt.ylabel(r'S')
+    plt.xlabel('Period (days)')
+    #plt.show()
+    plt.savefig('test_stat_tls_SDE_power.pdf', bbox_inches='tight')
+    print(min(results.SDE_power), max(results.chi2red))
 
 
     """
@@ -175,8 +210,8 @@ if __name__ == "__main__":
     flux = y_filt[sort_index]
     #flux = (flux - 1) * 10**6
     model_flux = (results.folded_model)# - 1) * 10**6
-    #plt.rc('font',  family='serif', serif='Computer Modern Roman')
-    #plt.rc('text', usetex=True)
+    plt.rc('font',  family='serif', serif='Computer Modern Roman')
+    plt.rc('text', usetex=True)
     ax.get_yaxis().set_tick_params(which='both', direction='out')
     ax.get_xaxis().set_tick_params(which='both', direction='out')
     plt.figure(figsize=(3.75, 3.75 / 1.5))
