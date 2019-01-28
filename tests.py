370e53134b2f119ec5e6f7cd0457a9aa51689832
Michael Hippke <hippke@users.noreply.github.com>
2019-01-28 15:22:59 +0100

    Add test for resample

--- tests.py
+++ tests.py
@@ -10,6 +10,7 @@ from transitleastsquares import (
     FAP,
     cleaned_array,
     transit_mask,
+    resample
 )
 
 def loadfile(filename):
@@ -24,6 +25,46 @@ def loadfile(filename):
 if __name__ == "__main__":
     numpy.random.seed(seed=0)  # reproducibility
     print("Starting tests. This should take less than one minute...")
+    
+    # Resampling
+    testtime = numpy.linspace(0, 1, 1000)
+    testflux = numpy.linspace(0.99, 1.01, 1000)
+    a, b = resample(time=testtime, flux=testflux, factor=100)
+    expected_result_a = (
+        0.,
+        0.11111111,
+        0.22222222,
+        0.33333333,
+        0.44444444,
+        0.55555556,
+        0.66666667,
+        0.77777778,
+        0.88888889,
+        1.
+        )
+    numpy.testing.assert_almost_equal(a, expected_result_a)
+    expected_result_b = (
+        0.99,
+        0.99222222,
+        0.99444444,
+        0.99666667,
+        0.99888889,
+        1.00111111,
+        1.00333333,
+        1.00555556,
+        1.00777778,
+        1.01
+        )
+    numpy.testing.assert_almost_equal(b, expected_result_b)
+    numpy.testing.assert_equal(len(a), 10)
+    numpy.testing.assert_almost_equal(min(a), 0)
+    numpy.testing.assert_almost_equal(max(a), 1)
+    numpy.testing.assert_almost_equal(numpy.mean(a), 0.5)
+    numpy.testing.assert_equal(len(b), 10)
+    numpy.testing.assert_almost_equal(min(b), 0.99)
+    numpy.testing.assert_almost_equal(max(b), 1.01)
+    numpy.testing.assert_almost_equal(numpy.mean(b), 1)
+    print('Test passed: Resampling')
 
     # Clean array test
     dirty_array = numpy.ones(10, dtype=object)
@@ -110,7 +151,7 @@ if __name__ == "__main__":
     # 394137592
     # 261136679
 
-    """
+    
     (a, b), mass, mass_min, mass_max, radius, radius_min, radius_max = catalog_info(
         TIC_ID=279741377
     )
@@ -160,7 +201,7 @@ if __name__ == "__main__":
     numpy.testing.assert_almost_equal(radius_min, 2.304)
 
     print("Test passed: KIC catalog pull from Vizier using astroquery")
-    """
+    
 
     # Create test data
     start = 48
